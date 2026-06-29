9079edc86e2db5161883dd49f6b05f2c4b7429db
MarcosWillianAP1K <mwss@outlook.com>
2026-06-29 18:09:26 -0300

    fix: remove pino-pretty completamente para o deploy na vercel

--- Backend/src/shared/errors/index.ts
+++ Backend/src/shared/errors/index.ts
@@ -2,16 +2,5 @@ import pino from "pino";
 import { env } from "../../config/env";
 
 export const logger = pino({
-  ...(env.isDev && {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-        translateTime: "2024-01-01T00:00:00.000Z",
-        singleLine: true,
-        ignore: "pid,hostname",
-        messageFormat: "{levelLabel}: {msg}",
-      },
-    },
-  }),
+  level: "info",
 });
