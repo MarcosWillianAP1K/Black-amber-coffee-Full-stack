b0543848fa103fbfa106c83976978b405ce48de2
MarcosWillianAP1K <mwss@outlook.com>
2026-06-29 18:05:36 -0300

    fix: desativa pino-pretty no ambiente de producao

--- Backend/src/shared/errors/index.ts
+++ Backend/src/shared/errors/index.ts
@@ -2,16 +2,16 @@ import pino from "pino";
 import { env } from "../../config/env";
 
 export const logger = pino({
-  transport: env.isDev
-    ? {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-          translateTime: "2024-01-01T00:00:00.000Z",
-          singleLine: true,
-          ignore: "pid,hostname",
-          messageFormat: "{levelLabel}: {msg}",
-        },
-      }
-    : undefined,
+  ...(env.isDev && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "2024-01-01T00:00:00.000Z",
+        singleLine: true,
+        ignore: "pid,hostname",
+        messageFormat: "{levelLabel}: {msg}",
+      },
+    },
+  }),
 });
